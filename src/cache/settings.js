9c780825092b29f7e12df9f8a3cdcc422a6390e9
RandomKiddo <nghugare2@outlook.com>
2023-01-15 20:25:23 -0500

    Prepare Settings HTML 2

--- src/cache/settings.js
+++ src/cache/settings.js
@@ -1,4 +1,29 @@
-const fs = require("fs");
+var fs = require("fs");
+
+function loadDefaultSettings() {
+    fs.readFile("./SETTINGS.json", "utf8", (err, jsonString) => {
+        if (err) {
+            console.log("File read error: ", err);
+            return;
+        }
+        try {
+            var data = JSON.parse(jsonString);
+        } catch (err) {
+            console.log(err);
+            return;
+        }
+        var incognitoSwitch = document.getElementById("incognitoSwitch");
+        incognitoSwitch.checked = data.incognito;
+    });
+}
 
 function switchChangeState(element) {
+    element.checked = !element.checked;
+    fs.readFile("./SETTINGS.json", "utf8", (err, jsonString) => {
+        if (err) {
+            console.log("File read error: ", err);
+            return;
+        }
+        // write to file
+    });
 }
